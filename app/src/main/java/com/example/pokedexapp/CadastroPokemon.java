22a80daa20d0a678706568859eb317ad536ba558
janaercole <janaercole@gmail.com>
2023-01-29 22:07:43 -0300

    estrutura inicial

---
+++ app/src/main/java/com/example/pokedexapp/CadastroPokemon.java
@@ -0,0 +1,14 @@
+package com.example.pokedexapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastroPokemon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_pokemon);
+    }
+}
