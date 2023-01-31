b4a7d061e905e9f441928814a1720fbbc4f58c0d
janaercole <janaercole@gmail.com>
2023-01-30 21:54:45 -0300

    menu

--- app/src/main/java/com/example/pokedexapp/ListarTodos.java
+++ app/src/main/java/com/example/pokedexapp/ListarTodos.java
@@ -1,14 +1,77 @@
 package com.example.pokedexapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ListarTodos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_listar_todos2);
     }
+
+
 }
+    /*public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+
+        private String[] localDataSet;
+
+        /**
+         * Provide a reference to the type of views that you are using
+         * (custom ViewHolder).
+         */
+       /* public static class ViewHolder extends RecyclerView.ViewHolder {
+            private final TextView textView;
+
+            public ViewHolder(View view) {
+                super(view);
+                // Define click listener for the ViewHolder's View
+
+                textView = (TextView) view.findViewById(R.id.textView);
+            }
+
+            public TextView getTextView() {
+                return textView;
+            }
+        }
+
+        /**
+         * Initialize the dataset of the Adapter.
+         *
+         * @param dataSet String[] containing the data to populate views to be used
+         * by RecyclerView.
+         */
+       /* public CustomAdapter(String[] dataSet) {
+            localDataSet = dataSet;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+            // Create a new view, which defines the UI of the list item
+            View view = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.text_row_item, viewGroup, false);
+
+            return new ViewHolder(view);
+        }
+
+
+    }
+
+
+*/
+
+
+
+
+
---
+++ app/src/main/java/com/example/pokedexapp/PesquisarHabilidade.java
@@ -0,0 +1,14 @@
+package com.example.pokedexapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PesquisarHabilidade extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pesquisar_habilidade);
+    }
+}
